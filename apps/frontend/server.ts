dd19990d76abd7cfa2812b08395c780d6ebbb178
Fred Rios <fredjhosnickrios@gmail.com>
2024-10-26 23:06:59 +0200

    Added files for memberspot challenge

--- apps/frontend/server.ts
+++ apps/frontend/server.ts
@@ -1,8 +1,8 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
-import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import bootstrap from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
@@ -54,7 +54,7 @@ function run(): void {
   const server = app();
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+  });;
 }
 
 run();
